157ed6ac98d326232cec2e7fc10b14026ac52f6f
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-01-21 21:58:32 -0200

    Initial Swagger config

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flasgger import Swagger
 
 from api import backend
 
@@ -6,5 +7,15 @@ def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(backend.api_bp, url_prefix=f'/api')
+
+    app.config['SWAGGER'] = {
+        'uiversion': 3,
+        'swagger_version': '3.0',
+        'title': 'Pharmacy Stock API',
+        'specs_route': '/docs',
+        'description': 'Routes to administrate API'
+    }
+
+    Swagger(app=app)
 
     return app
