ad9628401e43d55c0d8c58ae78864a7b6e27513d
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-01-21 21:44:22 -0200

    Create a function to make a request to application itself

--- src/api/backend/utils.py
+++ src/api/backend/utils.py
@@ -1,6 +1,8 @@
 import os
 import json
+import requests
 
+from flask import request
 from datetime import date, datetime
 from decimal import Decimal
 
@@ -16,3 +18,18 @@ def _json_encoder(obj):
 
 def _json_result(result):
     return json.loads(json.dumps(result, default=_json_encoder))
+
+
+def request_to_myself(verb, route, params=None, data=None,
+                      headers={}, add_token=True):
+    if add_token:
+        headers.update({'Token': request.headers.get('Token', '')})
+    
+    data = json.dumps(data) if data else data
+    response = requests.request(
+        verb,
+        f'http://127.0.0.1:5000/api{route}',
+        params=params,
+        data=data,
+        headers=headers)
+    return response
